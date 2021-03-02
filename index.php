ae7b508262554ed23278fc093084738bb1a415c9
Eby-Samuel693 <coquelet.samuel@gmail.com>
2021-03-02 10:41:16 +0100

    ujyhgtrfede

--- index.php
+++ index.php
@@ -14,47 +14,199 @@
  * -- Sélections complexes --
  * Une seule requête est permise pour chaque point de l'exo.
  */
-
+try {
 // TODO Commencez par créer votre objet de connexion à la base de données, vous pouvez aussi utiliser l'objet statique ou autre qu'on a créé ensemble.
-
-/* 1. Sélectionnez et affichez tous les utilisateurs dont le nom est 'Conor' */
+    $server = 'localhost';
+    $db = '194_table';
+    $user = 'root';
+    $pass = '';
+    /* 1. Sélectionnez et affichez tous les utilisateurs dont le nom est 'Conor' */
 // TODO votre code ici.
 
-/* 2. Sélectionnez et affichez tous les utilisateurs dont le prénom est différent de 'John' */
+    $bdd = new PDO("mysql:host=$server;dbname=$db;charset=utf8",$user,$pass);
+//    $stmt = $bdd->prepare("SELECT * from user WHERE nom='Conor'");
+//    $state= $stmt->execute();
+//    if($state){
+//        foreach ($stmt->fetchAll() as $user) {
+//            echo "Utilisateur: id ->".$user['id']."nom ->". $user['nom']."prenom ->" . $user['prenom']."<br>";
+//            echo "<div class='utilisateur'>";
+//            echo $user['nom']." ".$user['prenom']."<br>";
+//        }
+//    }
+
+    /* 2. Sélectionnez et affichez tous les utilisateurs dont le prénom est différent de 'John' */
 // TODO Votre code ici.
 
-/* 3. Sélectionnez et affichez tous les utilisateurs dont l'id est plus petit ou égal à 2 */
+//    $stmt = $bdd->prepare("SELECT * from user WHERE nom !='John'");
+//    $state= $stmt->execute();
+//    if($state){
+//        foreach ($stmt->fetchAll() as $user) {
+//            echo "Utilisateur: id ->".$user['id']."nom ->".$user['nom']."prenom ->". $user['prenom']."<br>";
+//            echo "<div class='utilisateur'>";
+//            echo $user['nom'] . " " . $user['prenom'] . "<br>";
+//        }
+//    }
+    /* 3. Sélectionnez et affichez tous les utilisateurs dont l'id est plus petit ou égal à 2 */
 // TODO Votre code ici.
 
-/* 4. Sélectionnez et affichez tous les utilisateurs dont l'id est plus grand ou égal à 2 */
+//    $stmt = $bdd->prepare("SELECT * from user WHERE id <= 2");
+//    $state= $stmt->execute();
+//    if($state){
+//        foreach ($stmt->fetchAll() as $user) {
+//            echo "Utilisateur: id ->".$user['id']."nom ->".$user['nom']."prenom ->". $user['prenom']."<br>";
+//            echo "<div class='utilisateur'>";
+//            echo $user['nom'] . " " . $user['prenom'] . "<br>";
+//        }
+//    }
+
+    /* 4. Sélectionnez et affichez tous les utilisateurs dont l'id est plus grand ou égal à 2 */
 // TODO Votre code ici.
 
-/* 5. Sélectionnez et affichez tous les utilisateurs dont l'id est égal à 1 */
+//    $stmt = $bdd->prepare("SELECT * from user WHERE id >= 2");
+//    $state= $stmt->execute();
+//    if($state){
+//        foreach ($stmt->fetchAll() as $user) {
+//            echo "Utilisateur: id ->".$user['id']."nom ->".$user['nom']."prenom ->". $user['prenom']."<br>";
+//            echo "<div class='utilisateur'>";
+//            echo $user['nom'] . " " . $user['prenom'] . "<br>";
+//        }
+//    }
+
+    /* 5. Sélectionnez et affichez tous les utilisateurs dont l'id est égal à 1 */
 // TODO Votre code ici.
 
-/* 6. Sélectionnez et affichez tous les utilisateurs dont l'id est plus grand que 1 ET le nom est égal à 'Doe' */
+//    $stmt = $bdd->prepare("SELECT * from user WHERE id =1");
+//    $state= $stmt->execute();
+//    if($state){
+//        foreach ($stmt->fetchAll() as $user) {
+//            echo "Utilisateur: id ->".$user['id']."nom ->".$user['nom']."prenom ->". $user['prenom']."<br>";
+//            echo "<div class='utilisateur'>";
+//            echo $user['nom'] . " " . $user['prenom'] . "<br>";
+//        }
+//    }
+
+    /* 6. Sélectionnez et affichez tous les utilisateurs dont l'id est plus grand que 1 ET le nom est égal à 'Doe' */
 // TODO Votre code ici.
 
-/* 7. Sélectionnez et affichez tous les utilisateurs dont le nom est 'Doe' ET le prénom est 'John'*/
+//    $stmt = $bdd->prepare("SELECT * from user WHERE id < 1 AND nom= 'Doe'");
+//    $state= $stmt->execute();
+//    if($state){
+//        foreach ($stmt->fetchAll() as $user) {
+//            echo "Utilisateur: id ->".$user['id']."nom ->".$user['nom']."prenom ->". $user['prenom']."<br>";
+//            echo "<div class='utilisateur'>";
+//            echo $user['nom'] . " " . $user['prenom'] . "<br>";
+//        }
+//    }
+
+    /* 7. Sélectionnez et affichez tous les utilisateurs dont le nom est 'Doe' ET le prénom est 'John'*/
 // TODO Votre code ici.
 
-/* 8. Sélectionnez et affichez tous les utilisateurs dont le nom est 'Conor' OU le prénom est 'Jane' */
+//    $stmt = $bdd->prepare("SELECT * from user WHERE nom='Doe' AND prenom='John'");
+//    $state= $stmt->execute();
+//    if($state){
+//        foreach ($stmt->fetchAll() as $user) {
+//            echo "Utilisateur: id ->".$user['id']."nom ->".$user['nom']."prenom ->". $user['prenom']."<br>";
+//            echo "<div class='utilisateur'>";
+//            echo $user['nom'] . " " . $user['prenom'] . "<br>";
+//        }
+//    }
+
+    /* 8. Sélectionnez et affichez tous les utilisateurs dont le nom est 'Conor' OU le prénom est 'Jane' */
 // TODO Votre code ici.
 
-/* 9. Sélectionnez et affichez tous les utilisateurs en limitant les réultats à 2 résultats */
+//    $stmt = $bdd->prepare("SELECT * from user WHERE nom='Conor' OR prenom='Jane'");
+//    $state= $stmt->execute();
+//    if($state){
+//        foreach ($stmt->fetchAll() as $user) {
+//            echo "Utilisateur: id ->".$user['id']."nom ->".$user['nom']."prenom ->". $user['prenom']."<br>";
+//            echo "<div class='utilisateur'>";
+//            echo $user['nom'] . " " . $user['prenom'] . "<br>";
+//        }
+//    }
+
+    /* 9. Sélectionnez et affichez tous les utilisateurs en limitant les réultats à 2 résultats */
 // TODO Votre code ici.
 
-/* 10. Sélectionnez et affichez tous les utilisateurs par ordre croissant, en limitant le résultat à 1 seul enregistrement */
+//    $stmt = $bdd->prepare("SELECT * from user LIMIT 2");
+//    $state= $stmt->execute();
+//    if($state){
+//        foreach ($stmt->fetchAll() as $user) {
+//            echo "Utilisateur: id ->".$user['id']."nom ->".$user['nom']."prenom ->". $user['prenom']."<br>";
+//            echo "<div class='utilisateur'>";
+//            echo $user['nom'] . " " . $user['prenom'] . "<br>";
+//        }
+//    }
+
+    /* 10. Sélectionnez et affichez tous les utilisateurs par ordre croissant, en limitant le résultat à 1 seul
+     enregistrement */
 // TODO Votre code ici.
 
-/* 11. Sélectionnez et affichez tous les utilisateurs dont le nom commence par C, fini par r et contient 5 caractères ( voir LIKE )*/
+//    $stmt = $bdd->prepare("SELECT * from user  ORDER BY id DESC LIMIT 1");
+//    $state= $stmt->execute();
+//    if($state){
+//        foreach ($stmt->fetchAll() as $user) {
+//            echo "Utilisateur: id ->".$user['id']."nom ->".$user['nom']."prenom ->". $user['prenom']."<br>";
+//            echo "<div class='utilisateur'>";
+//            echo $user['nom'] . " " . $user['prenom'] . "<br>";
+//        }
+//    }
+
+    /* 11. Sélectionnez et affichez tous les utilisateurs dont le nom commence par C,
+     fini par r et contient 5 caractères ( voir LIKE )*/
 // TODO Votre code ici.
 
-/* 12. Sélectionnez et affichez tous les utilisateurs dont le nom contient au moins un 'e' */
+//    $stmt = $bdd->prepare("SELECT * from user WHERE nom LIKE'C%'AND nom LIKE '%r'");
+//    $state= $stmt->execute();
+//    if($state){
+//        foreach ($stmt->fetchAll() as $user) {
+//            echo "Utilisateur: id ->".$user['id']."nom ->".$user['nom']."prenom ->". $user['prenom']."<br>";
+//            echo "<div class='utilisateur'>";
+//            echo $user['nom'] . " " . $user['prenom'] . "<br>";
+//        }
+//    }
+
+    /* 12. Sélectionnez et affichez tous les utilisateurs dont le nom contient au moins un 'e' */
 // TODO Votre code ici.
 
-/* 13. Sélectionnez et affichez tous les utilisateurs dont le prénom est ( IN ) (John, Sarah) ... voir IN , pas OR '' */
+//    $stmt = $bdd->prepare("SELECT * from user WHERE nom LIKE '%e%'");
+//    $state= $stmt->execute();
+//    if($state){
+//        foreach ($stmt->fetchAll() as $user) {
+//            echo "Utilisateur: id ->".$user['id']."nom ->".$user['nom']."prenom ->". $user['prenom']."<br>";
+//            echo "<div class='utilisateur'>";
+//            echo $user['nom'] . " " . $user['prenom'] . "<br>";
+//        }
+//    }
+
+    /* 13. Sélectionnez et affichez tous les utilisateurs dont le prénom est ( IN ) (John, Sarah) ...
+     voir IN , pas OR '' */
 // TODO Votre code ici.
 
-/* 14. Sélectionnez et affichez tous les utilisateurs dont l'id est situé entre 2 et 4 */
+//    $stmt = $bdd->prepare("SELECT * from user WHERE prenom IN ('John','Sarah')");
+//    $state= $stmt->execute();
+//    if($state){
+//        foreach ($stmt->fetchAll() as $user) {
+//            echo "Utilisateur: id ->".$user['id']."nom ->".$user['nom']."prenom ->". $user['prenom']."<br>";
+//            echo "<div class='utilisateur'>";
+//            echo $user['nom'] . " " . $user['prenom'] . "<br>";
+//        }
+//    }
+
+    /* 14. Sélectionnez et affichez tous les utilisateurs dont l'id est situé entre 2 et 4 */
 // TODO Votre code ici.
+
+    $stmt = $bdd->prepare("SELECT * from user WHERE id <=2 AND id >=4");
+    $state= $stmt->execute();
+    if($state){
+        foreach ($stmt->fetchAll() as $user) {
+            echo "Utilisateur: id ->".$user['id']."nom ->".$user['nom']."prenom ->". $user['prenom']."<br>";
+            echo "<div class='utilisateur'>";
+            echo $user['nom'] . " " . $user['prenom'] . "<br>";
+        }
+    }
+
+}
+
+catch (PDOException $exception) {
+    echo $exception->getMessage();
+}
